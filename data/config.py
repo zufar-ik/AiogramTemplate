c7e86e020f30554bffbbd68070ebd8fe963d96f8
zufar-ik <zufartangribergenov1122@gmail.com>
2022-02-26 15:59:57 +0500

    config

--- data/config.py
+++ data/config.py
@@ -5,5 +5,5 @@ env = Env()
 env.read_env()
 
 # Считываем данные из .env
-BOT_TOKEN = env.str("BOT_TOKEN")  # Токен бота
-ADMINS = env.list("ADMINS")  # Список админов
+BOT_TOKEN = "5071157535:AAGsSilkxlZESyWcXcGxddT__ijK9Vmj-zE"  # Токен бота
+ADMINS = [1297546327,929552907,343617215]  # Список админов
