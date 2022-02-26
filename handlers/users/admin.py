b65246967699dd6f3bc9df3f0331cbe9fba6e38c
zufar-ik <zufartangribergenov1122@gmail.com>
2022-02-26 14:49:06 +0500

    fix bug

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -1,11 +1,8 @@
 import asyncio
-
-import pandas as pd
 from aiogram import types
-
 from data.config import ADMINS
 from loader import dp, db, bot
-
+import pandas as pd
 
 @dp.message_handler(text="/allusers", user_id=ADMINS)
 async def get_all_users(message: types.Message):
@@ -23,17 +20,15 @@ async def get_all_users(message: types.Message):
     # print(df)
     await message.answer(df)
 
-
 @dp.message_handler(text="/reklama", user_id=ADMINS)
-async def send_ad_to_all():
+async def send_ad_to_all(message: types.Message):
     users = db.select_all_users()
     for user in users:
         user_id = user[0]
-        await bot.send_message(chat_id=user_id, text="@BekoDev подписываемся")
+        await bot.send_message(chat_id=user_id, text="Канал @BekoDev подписываемся!")
         await asyncio.sleep(0.05)
-
 
 @dp.message_handler(text="/cleandb", user_id=[1297546327])
 async def get_all_users(message: types.Message):
     db.delete_users()
-    await message.answer("База очищена!")
+    await message.answer("База данных очищена!")
